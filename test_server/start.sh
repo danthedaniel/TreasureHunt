d987c85b7e0d7bdaac24f3c3c53787f740ec464d
Daniel Angell <me@danangell.com>
2024-02-06 14:33:46 -0500

    Update to 1.20.4

--- test_server/start.sh
+++ test_server/start.sh
@@ -2,8 +2,8 @@
 
 cd $(dirname $0)
 
-MC_VERSION='1.20.2'
-PAPER_BUILD='223'
+MC_VERSION='1.20.4'
+PAPER_BUILD='409'
 PAPER_JAR_URL="https://api.papermc.io/v2/projects/paper/versions/$MC_VERSION/builds/$PAPER_BUILD/downloads/paper-$MC_VERSION-$PAPER_BUILD.jar"
 
 if [ ! -f paper.jar ]; then
